5f9662a9639b02576cea67c2f148cfdd04104995
lucianafalcon <lfalcon@fi.uba.ar>
2025-10-06 17:51:12 -0300

    creacion db y tablas sql

---
+++ tablas.sql
@@ -0,0 +1,66 @@
+/* ==========================================================
+   Medicinalis - L.Falcon
+   
+   Script de creación de base de datos y tablas Datos Iniciales
+========================================================== */
+
+-- Eliminar tablas si existen
+DROP TABLE IF EXISTS paciente;
+DROP TABLE IF EXISTS estudio;
+DROP TABLE IF EXISTS resultado;
+DROP TABLE IF EXISTS biomarcador;
+DROP TABLE IF EXISTS condicion;
+DROP TABLE IF EXISTS recomendacion;
+
+-- creacion tablas
+CREATE TABLE paciente (
+    id_paciente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    fecha_nacimiento DATE,
+    sexo VARCHAR(10)
+);
+
+CREATE TABLE estudio (
+    id_estudio INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_estudio VARCHAR(50),
+    fecha_estudio DATE
+);
+
+CREATE TABLE resultado (
+    id_resultado INT AUTO_INCREMENT PRIMARY KEY,
+    id_paciente INT,
+	id_estudio INT,
+    id_biomarcador INT, 
+    rango_min INT,
+    rango_max INT,
+    FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente),
+    FOREIGN KEY (id_estudio) REFERENCES estudio(id_estudio),
+    FOREIGN KEY (id_biomarcador) REFERENCES biomarcador(id_biomarcador)
+);
+
+CREATE TABLE biomarcador (
+    id_biomarcador INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_biomarcador VARCHAR(50),
+    unidad_medida VARCHAR(10)
+);
+
+CREATE TABLE condicion (
+    id_condicion INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_condicion VARCHAR(50)
+);
+
+CREATE TABLE recomendacion (
+    id_recomendacion INT AUTO_INCREMENT PRIMARY KEY,
+	id_condicion INT,
+    producto_recomendado VARCHAR(50),
+    descripcion_recomendacion VARCHAR(100),
+    FOREIGN KEY (id_condicion) REFERENCES condicion(id_condicion)
+);
+
+
+
+
+
+
+
