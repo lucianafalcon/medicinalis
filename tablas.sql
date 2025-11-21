c628254d7f006b26911783cc038c0016d1028499
Ciana <lfalcon@fi.uba.ar>
2025-11-21 06:00:35 -0300

    Add new tables for nutritional results and recommendations

--- tablas.sql
+++ tablas.sql
@@ -83,3 +83,70 @@ CREATE TABLE Resultado_Condicion (
     FOREIGN KEY (id_resultado) REFERENCES Resultado(id_resultado), 
     FOREIGN KEY (id_condicion) REFERENCES Condicion(id_condicion)
 );
+
+
+-- ######################################
+-- 2. TABLA DE IMPLEMENTACION
+-- ######################################
+
+-- TABLA 8 — Hecho: ResultadosNutricionales
+CREATE TABLE Hecho_ResultadosNutricionales (
+    id_hecho INT AUTO_INCREMENT PRIMARY KEY,
+    id_paciente INT,
+    id_estudio INT,
+    id_biomarcador INT,
+    valor_resultado FLOAT,
+    diferencia_min FLOAT,
+    diferencia_max FLOAT,
+    fecha_estudio DATE,
+    FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente),
+    FOREIGN KEY (id_estudio) REFERENCES Estudio(id_estudio),
+    FOREIGN KEY (id_biomarcador) REFERENCES Biomarcador(id_biomarcador)
+);
+
+
+-- TABLA 9 — Transaccional: TransaccionEstudio
+CREATE TABLE Transaccion_Estudio (
+    id_transaccion_estudio INT AUTO_INCREMENT PRIMARY KEY,
+    id_estudio INT,
+    fecha_transaccion DATETIME,
+    accion VARCHAR(50),
+    FOREIGN KEY (id_estudio) REFERENCES Estudio(id_estudio)
+);
+
+-- TABLA 10 — UnidadMedida
+CREATE TABLE UnidadMedida (
+    id_unidad INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_unidad VARCHAR(20)
+);
+
+-- TABLA 11 — CategoriaBiomarcador
+CREATE TABLE CategoriaBiomarcador (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(50)
+);
+
+-- TABLA 12 — Biomarcador_Categoria 
+CREATE TABLE Biomarcador_Categoria (
+    id_biomarcador INT,
+    id_categoria INT,
+    PRIMARY KEY (id_biomarcador, id_categoria),
+    FOREIGN KEY (id_biomarcador) REFERENCES Biomarcador(id_biomarcador),
+    FOREIGN KEY (id_categoria) REFERENCES CategoriaBiomarcador(id_categoria)
+);
+
+-- TABLA 13 — CategoriaRecomendacion
+CREATE TABLE CategoriaRecomendacion (
+    id_categoria_reco INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(50)
+);
+
+
+-- TABLA 14 — Recomendacion_Categoria 
+CREATE TABLE Recomendacion_Categoria (
+    id_recomendacion INT,
+    id_categoria_reco INT,
+    PRIMARY KEY (id_recomendacion, id_categoria_reco),
+    FOREIGN KEY (id_recomendacion) REFERENCES Recomendacion(id_recomendacion),
+    FOREIGN KEY (id_categoria_reco) REFERENCES CategoriaRecomendacion(id_categoria_reco)
+);
